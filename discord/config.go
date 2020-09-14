85ada29afa2a94b3dbc0a0e3fce3cb01125bf852
Juan Alvarez <juan@nemphi.com>
2020-09-14 18:13:48 -0500

    Have a DefaultConfig for the bot

--- discord/config.go
+++ discord/config.go
@@ -4,4 +4,12 @@ package discord
 type Config struct {
 	Token      string
 	InviteLink string
+	Prefix     string
 }
+
+var (
+	// DefaultConfig for a discord bot
+	DefaultConfig = Config{
+		Prefix: "#!",
+	}
+)
