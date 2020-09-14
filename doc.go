6f522a4b478f63493cf49c18b265aab9587abcd8
Juan Alvarez <juan@nemphi.com>
2020-09-14 18:13:31 -0500

    Start platform-agnostic structure

---
+++ doc.go
@@ -0,0 +1,6 @@
+// Package sento is a platform-agnostic chat bot framework.
+//
+// It supports:
+// 	Discord
+// 	Slack
+package sento
--- types.go
+++ types.go
@@ -1,8 +1,23 @@
 package sento
 
-// Bot is a sento-powered bot application
-type Bot interface {
-	New()
-	Start() error
-	Stop() error
+import (
+	"log"
+	"os"
+)
+
+// Bot is a sento-powered bot
+type Bot struct {
+	logger *log.Logger
+}
+
+// New sento-powered bot
+func New() Bot {
+	return Bot{
+		logger: log.New(os.Stdout, "[SentoBot]", log.LstdFlags),
+	}
+}
+
+// LogInfo logs the `msg` to the console
+func (b Bot) LogInfo(msg string) {
+	b.logger.Println(msg)
 }
