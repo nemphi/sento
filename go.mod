0ee84ad2e85ac5f93d63b3b493d2bffa51b975f2
Juan Alvarez <juan@nemphi.com>
2020-09-14 22:35:06 -0500

    Use go.uber.org/zap as the logging library

--- go.mod
+++ go.mod
@@ -5,4 +5,5 @@ go 1.14
 require (
 	github.com/BurntSushi/toml v0.3.1
 	github.com/bwmarrin/discordgo v0.22.0
+	go.uber.org/zap v1.16.0
 )
--- types.go
+++ types.go
@@ -1,23 +1,31 @@
 package sento
 
 import (
-	"log"
-	"os"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
 )
 
 // Bot is a sento-powered bot
 type Bot struct {
-	logger *log.Logger
+	logger *zap.Logger
 }
+
+// LogField for a logger
+type LogField = zap.Field
+
+// LogFieldType is used when defining fields
+type LogFieldType = zapcore.FieldType
 
 // New sento-powered bot
 func New() Bot {
+	// TODO: Don't ignore the error
+	logger, _ := zap.NewProduction()
 	return Bot{
-		logger: log.New(os.Stdout, "[SentoBot]", log.LstdFlags),
+		logger: logger,
 	}
 }
 
 // LogInfo logs the `msg` to the console
-func (b Bot) LogInfo(msg string) {
-	b.logger.Println(msg)
+func (b Bot) LogInfo(msg string, fields ...LogField) {
+	b.logger.Info(msg, fields...)
 }
