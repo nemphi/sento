538f1bf4249edc2296da61ad7f62801173b4adf7
Juan Alvarez <juan@nemphi.com>
2020-09-20 14:11:47 -0500

    Emit `EventMessageSent` event

--- helpers.go
+++ helpers.go
@@ -2,6 +2,12 @@ package sento
 
 // Send `message` into the given channel
 func (bot *Bot) Send(info HandleInfo, message string) (err error) {
-	_, err = bot.Sess.ChannelMessageSend(info.ChannelID, message)
+	msg, err := bot.Sess.ChannelMessageSend(info.ChannelID, message)
+	handleInfo := HandleInfo{
+		AuthorID:  msg.Author.ID,
+		ChannelID: msg.ChannelID,
+		GuildID:   msg.GuildID,
+	}
+	bot.EmitEvent(EventMessageSent, handleInfo)
 	return
 }
